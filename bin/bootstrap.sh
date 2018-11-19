12ae203ba61714fcb7ae1b109b35ac625a2cd108
rinshadka <rinshad55@gmail.com>
2018-11-19 10:48:05 +0000

    Modify appliance configuration to include FHIR-Sync specific informations.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -24,10 +24,14 @@ title "Prepare appliance secrets configuration"
 sudo cp $ASF_HOME/conf/appliance.secrets.conf-tmpl.yml $ASF_HOME/conf/appliance.secrets.conf.yml
 
 title "Prepare wellsky cprplus configuration"
-sudo cp $ASF_HOME/conf/wellsky-cprplus.secrets.conf-tmpl.json $ASF_HOME/conf/wellsky-cprplus.secrets.conf.json
+sudo cp $ASF_HOME/conf/wellsky-cprplus.secrets.conf-tmpl.jsonnet $ASF_HOME/conf/wellsky-cprplus.secrets.conf.jsonnet
+
+title "Prepare wellsky fhirsync configuration"
+sudo cp $ASF_HOME/conf/wellsky-fhir-sync.conf-tmpl.jsonnet $ASF_HOME/conf/wellsky-fhir-sync.conf.jsonnet
 
 title "Prepare appliance domain for traefik label configuration"
 sudo cp $ASF_HOME/conf/appliance.domain.conf-tmpl.json $ASF_HOME/conf/appliance.domain.conf.json
+
 
 title "Provision ARA setup playbook"
 sudo ansible-playbook -i "localhost," -c local $ASF_HOME/playbooks/ara.ansible-playbook.yml
---
+++ conf/wellsky-fhir-sync.conf-tmpl.jsonnet
@@ -0,0 +1,6 @@
+{
+   "wellsky_cprplus_FHIR_facade_url": "http://localhost:3000/3_0_1",
+   "interval_time_poll_wellsky_cprplus": "1",
+   "citus_FHIR_facade_url": "http://192.168.1.61:3000/3_0_1",
+   "interval_time_poll_citus": "1"
+}
