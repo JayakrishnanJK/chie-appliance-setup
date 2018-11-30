90d3a215566e37f13b559706156cbd02e7c23c75
Anish Varghese <anish.varghese@citrusinformatics.com>
2018-11-30 18:58:52 +0530

    Fixed the key server timeout issue due to firewall blockage

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,8 +7,10 @@ title() {
 
 title "Install osQuery"
 export OSQUERY_KEY=1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
-sudo add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
+sudo gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
+sudo gpg -a --export  1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B | sudo apt-key add -
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
+#sudo add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
 sudo apt-get update
 sudo apt-get install -y osquery
 
